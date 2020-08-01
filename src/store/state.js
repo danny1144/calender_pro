00b3f68b6b11c8e4712eb69475b8017f10dbb7c0
1490284462@qq.com <1490284462@qq.com>
2020-08-01 21:14:38 +0800

    :art:修改错误

--- src/store/state.js
+++ src/store/state.js
@@ -2,5 +2,5 @@ export default {
     headerTitle: '当天信息',
     placeholder: '请输入',
     maxlength: 8,
-    error_code:0
+    error_code: 0
 }
--- src/views/Day.vue
+++ src/views/Day.vue
@@ -1,28 +1,26 @@
 <template>
   <div class="container">
-      <day-card></day-card>
+    <day-card></day-card>
   </div>
 </template>
 <script>
 import getData from "../service";
-import DayCard from "/@/components/Card/index.vue"
-import { onMounted} from "vue";
-import {useStore} from 'vuex'
+import DayCard from "/@/components/Card/index.vue";
+import { onMounted } from "vue";
+import { useStore } from "vuex";
 export default {
   name: "DayPage",
-  components:{
-    DayCard
+  components: {
+    DayCard,
   },
   setup() {
-
     onMounted(() => {
-      const store=useStore()
-      getData("day", "2020-7-7");
+      const store = useStore();
+      getData(store, "day", "2020-7-7");
     });
   },
 };
 </script>
 
 <style lang='scss' scoped>
-      
 </style>
